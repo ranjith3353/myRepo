884f40b27fb99a9e923f61fd0a4ec4170ef67e8a
lranjith <lranjith@DESKTOP-PGEKLJP.pkrmsezdc.com>
2020-09-06 14:31:37 +0530

    merging with branch data

--- sql-project/bin/sample-file.sql
+++ sql-project/bin/sample-file.sql
@@ -1,5 +1,17 @@
-create table sample_table(name varchar(100), age int);
+--create table sample_table(name varchar(100), age int);
+
+select * from sample_table;
+
+insert into sample_table(name, age) values('example_user', 25);
 
 insert into sample_table(name, age) values('ranjith', 25);
+
+insert into sample_table(name, age) values('danny', 25);
+
+--select * from sample_table;
+
+delete from sample_table where name='danny';
+
+delete from sample_table where name='ranjith';
 
 select * from sample_table;
