a74122d03292cdc4309b59d5337a1fb9cfaf9551
lranjith <lranjith@iranjith-lp.pkrmsezdc.com>
2021-05-10 00:33:04 +0530

    Added new methods

--- java-training/src/main/java/training/java_training/basic_examples/FindPairOfValuesFromArray.java
+++ java-training/src/main/java/training/java_training/basic_examples/FindPairOfValuesFromArray.java
@@ -15,6 +15,8 @@ public class FindPairOfValuesFromArray {
 		
 		int expectedDifference = 4;
 		
+		int expectedProduct = 20;
+		
 		printSumCombinationsUsingTwoPointersTechnique(A, expectedSum);
 		System.out.println("===========================================");
 		
@@ -26,6 +28,70 @@ public class FindPairOfValuesFromArray {
 		
 		printDifferenceCombinationsUsingHashSet(A, expectedDifference);
 		System.out.println("===========================================");
+		
+		printMultiplicationCombinationsUsingHashSet(A, expectedProduct);
+		System.out.println("===========================================");
+		
+		printMultiplicationCombinationsUsingTwoPointersTechnique(A, expectedProduct);
+		System.out.println("===========================================");
+		
+		printPairsHavingProductEqualsToX(A);
+		System.out.println("===========================================");
+	}
+
+	private static void printMultiplicationCombinationsUsingTwoPointersTechnique(int[] intArray, int output) {
+		System.out.println("Sorting & two pointer technique for multiplication");
+		//Arrays.sort(intArray); // even if we don't sort, we can achieve the output
+		int left = 0;
+		int right = intArray.length-1;
+		int count = 0;
+		System.out.print("The pair of values equals expected sum: "+output+" are: ");
+		while(left < right) {
+			if (intArray[left] * intArray[right] == output) {
+				System.out.print("["+intArray[left] +","+intArray[right]+"]");
+				left++;
+				count++;
+			} else if(intArray[left] * intArray[right] < output) {
+				left++;
+			} else {
+				right--;
+			}
+		}
+		System.out.println();
+		System.out.println("Total pairs: "+ count);
+	}
+
+	private static void printMultiplicationCombinationsUsingHashSet(int[] intArray, int output) {
+		System.out.println("Hashing Technique for multiplication");
+		Set<Integer> s = new HashSet<Integer>();
+		int count = 0;
+		System.out.print("The pair of values equals expected product: "+output+" are: ");
+		for (int i = 0; i < intArray.length; ++i) {
+			int temp = output / intArray[i];
+			if(s.contains(temp)) {
+				System.out.print("["+intArray[i] +","+temp+"]");
+				count++;
+			}
+			s.add(intArray[i]);
+		}
+		System.out.println();
+		System.out.println("Total pairs: "+ count);
+		System.out.println("This approcah can't give all pairs - so try using two pointer technique");
+	}
+
+	private static void printPairsHavingProductEqualsToX(int[] a) {
+		System.out.println("Product of Sum & Difference equals X example");
+		int count = 0;
+		int x = 3;
+		System.out.print("The pairs are: ");
+		for (int i = 1, j = 0; i < a.length && i > j; i++,j++) {
+			if((a[i]+a[j])*(a[i]-a[j]) == x) {
+				System.out.print("["+a[j] +","+a[i]+"]");
+				count++;
+			}
+		}
+		System.out.println();
+		System.out.println("Total pairs: "+ count);
 	}
 
 	private static void printDifferenceCombinationsUsingTwoPointersTechnique(int[] intArray, int output) {
